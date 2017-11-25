1a8093e013212dc090918cf191a60c9977d2b880
yinsang <1175180338@qq.com>
2017-11-25 12:45:05 +0800

    add stash

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ console.log('add dev item');
 document.write('hello world, webpack;')
 console.log(1);
 document.write('hello world, webpack;1 22')
+console.log(1, 'ADD 2');
+document.write('hello world, webpack;1')
+
