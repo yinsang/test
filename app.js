09fd33cb3dc5b6ed73297b442297fc5ba0c59f11
yinsang <1175180338@qq.com>
2017-11-25 16:31:55 +0800

    Revert "add 1"
    This reverts commit b1c2399dc7d346c36ad409512bc565b0734b0b15.

--- app.js
+++ app.js
@@ -2,7 +2,4 @@ document.write('hello world, webpack;1')
 console.log('add dev item');
 document.write('hello world, webpack;')
 console.log(1);
-document.write('hello world, webpack;1 22 333')
-console.log(1, 'ADD 2');
-document.write('hello world, webpack;1')
-
+document.write('hello world, webpack;1 22')
