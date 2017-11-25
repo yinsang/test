b1c2399dc7d346c36ad409512bc565b0734b0b15
yinsang <1175180338@qq.com>
2017-11-25 12:45:05 +0800

    add 1

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ document.write('hello world, webpack;1')
 console.log('add dev item');
 document.write('hello world, webpack;')
 console.log(1);
-document.write('hello world, webpack;1 22')
+document.write('hello world, webpack;1 22 333')
+console.log(1, 'ADD 2');
+document.write('hello world, webpack;1')
+
