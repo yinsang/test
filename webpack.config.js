975f1e833ab1730279beb3229081b8dbafc040dc
魏银鹏 <weiyinpeng@meituan.com>
2017-12-05 16:43:38 +0800

    2

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var path = require("path");
 var assetsPath = path.join(__dirname, "public", "assets");
 var serverPath = path.join(__dirname, "server");
@@ -35,19 +34,3 @@ module.exports = [
         }
     }
 ]
-=======
-let path = require('path');
-module.exports = {
-    entry: './app.js',
-    output:{
-        path: __dirname + '/build',
-        filename: 'bundle.js'
-    },
-    devServer:{
-        compress: true,
-        port:7000,
-        open: true,
-        inline: true
-    }
-}
->>>>>>> 09fd33cb3dc5b6ed73297b442297fc5ba0c59f11
