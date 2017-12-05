c5356704a8b02906b8829a36e0e80467568dcc56
魏银鹏 <weiyinpeng@meituan.com>
2017-12-05 20:31:30 +0800

    add 2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.write('hello world, webpack;1')
 console.log('add dev item');
 document.write('hello world, webpack;')
-console.log(1);
+console.log(12);
 document.write('hello world, webpack;1 22')
